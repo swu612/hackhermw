646741a7b3afe595e4b35fc00bb3582dbb7a0765
Steven Wu <stevenwu@Stevens-MacBook-Pro.local>
2024-02-17 22:30:58 -0500

    help

--- ai/views.py
+++ ai/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from brain import generate_ideas, generate_stack, write_boilerplate
+from .import brain
 
 def gen_ideas(user_input: str):
-    response = generate_ideas(user_input)
+    response = brain.generate_ideas(user_input)
     return response 
 
 def gen_stack(user_input: str, level: str):
-    response = generate_stack(user_input)
+    response = brain.generate_stack(user_input)
     return response
 
 def gen_boilerplate(user_input: str, level: str, tech_stack: str):
-    response = write_boilerplate(user_input)
+    response = brain.write_boilerplate(user_input)
     return response
 
 
