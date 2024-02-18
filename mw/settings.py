7bec3cec2e03136f69e118092067240bec970ce3
Steven Wu <stevenwu@Stevens-MacBook-Pro.local>
2024-02-17 22:18:52 -0500

    help

--- mw/settings.py
+++ mw/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ai.myapp',
+    'ai',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
